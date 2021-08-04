d267fcaccc2e58a72173150699c8b710862c7b24
Vickordon <vickordon@gmail.com>
2021-08-04 09:13:25 +0300

    addModEnterprises

---
+++ database/migrations/2021_08_04_055629_create_enterprises_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEnterprisesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('enterprises', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('image');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('enterprises');
+
+        Schema::table('enterprises', function (Blueprint $table) {
+            $table->dropColumn('id');
+            $table->dropColumn('name');
+            $table->dropColumn('image');
+        });
+    }
+}

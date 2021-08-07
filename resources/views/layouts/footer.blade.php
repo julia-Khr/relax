c8a02adf466af62c92f55093ab37a3113867d60d
julia-Khr <pryadun.u.o@gmail.com>
2021-08-07 13:51:25 +0300

    Task creating footer

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,57 @@
+<footer class="text-white px-5 py-5" style="height: 300px; width: 100%;">
+    <div class="text-center text-md-left">
+    <div class="row text-center text-md-left">
+        <div class="col-4 mx-auto">
+            <ul class="list-unstyled text-start" style="font-family: 'Roboto', sans-serif;">
+                <li>Вінниця, Провулок Цегельний, 12 </li>
+                <li>Пн-Пт  10:00 - 19:00</li>
+                <li>Сб-Нд  11:00 - 19:00</li>
+
+            </ul>
+        </div>
+        <div class="col-4 mx-auto d-md-flex flex-row justify-content-between px-4">
+            <img src="/admin/dist/img/Vector.png" width="114px" height="93px">
+                 <h1 class="text-uppercase font-weight-bold" style="font-family: 'Montserrat', sans-serif; font-size: 53px; line-height: 114px">IT RELAX</h1>
+        </div>
+        <div class="col-4 mx-auto d-md-flex flex-md-row-reverse">
+            <ul class="list-unstyled " style="font-family: 'Roboto', sans-serif;">
+                <li>+38 067 431 74 24</li>
+                <li>+38 067 431 75 21</li>
+                <li>+38 067 431 75 22</li>
+            </ul>
+        </div>
+    </div>
+    <div class="row text-center text-md-left">
+        <div class="col-4 mx-auto text-md-left" style="font-family: 'Roboto', sans-serif;"><p>intitavn@gmail.com</p></div>
+        <div class="col-4 mx-auto">
+            <form class="search-wrapper cf">
+                <input type="text" class="form-control" placeholder="E-mail" aria-label="E-mail"
+                 style="border-radius: 20px; box-shadow: none">
+                <button type="submit">Відправити</button>
+            </form>
+        </div>
+        <div class="col-4">
+            <div class="text-center">
+                <ul class="list-unstyled list-inline text-md-right">
+                    <li class="list-inline-item ">
+                        <a href="#" class="btn-floating btn-sm text-orange" style="font-size: 23px;"><i class="fab fa-instagram"></i></a>
+                    </li>
+                    <li class="list-inline-item">
+                        <a href="#" class="btn-floating btn-sm text-orange" style="font-size: 23px;"><i class="fab fa-telegram"></i></a>
+                    </li>
+                    <li class="list-inline-item">
+                        <a href="#" class="btn-floating btn-sm text-orange" style="font-size: 23px;"><i class="fab fa-facebook"></i></a>
+                    </li>
+                    <li class="list-inline-item">
+                        <a href="#" class="btn-floating btn-sm text-orange" style="font-size: 23px;"><i class="fab fa-youtube"></i></a>
+                    </li>
+                    <li class="list-inline-item">
+                        <a href="#" class="btn-floating btn-sm text-orange" style="font-size: 23px;"><i class="fab fa-viber"></i></a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    </div>
+  </footer>
+
---
+++ resources/views/visitor/home/index.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <title>visitor @yield('title')</title>
+  @include('layouts.style')
+</head>
+<body>
+<div class="content" style="height: 100%">
+
+</div>
+@include('layouts.footer')
+
+@include('layouts.scripts')
+</body>
+</html>

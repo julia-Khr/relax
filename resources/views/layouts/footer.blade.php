b5344068ba6f7519175e6c4b9cbe929df09d668d
julia-Khr <pryadun.u.o@gmail.com>
2021-08-07 20:23:38 +0300

    task footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,17 +1,22 @@
-<footer class="text-white px-5 py-5" style="height: 300px; width: 100%;">
-    <div class="text-center text-md-left">
-    <div class="row text-center text-md-left">
-        <div class="col-4 mx-auto">
+<footer class="text-white px-5 py-5">
+    <div class="text-center">
+    <div class="row text-center ">
+        <div class="col-4 mx-auto text-md-left">
             <ul class="list-unstyled text-start" style="font-family: 'Roboto', sans-serif;">
-                <li>Вінниця, Провулок Цегельний, 12 </li>
+                <li style=>Вінниця, Провулок Цегельний, 12 </li>
                 <li>Пн-Пт  10:00 - 19:00</li>
                 <li>Сб-Нд  11:00 - 19:00</li>
 
             </ul>
         </div>
-        <div class="col-4 mx-auto d-md-flex flex-row justify-content-between px-4">
+        <div class="col-4 mx-auto d-md-flex flex-column">
+            <div class="mx-auto d-md-flex flex-row justify-content-between px-4">
             <img src="/admin/dist/img/Vector.png" width="114px" height="93px">
                  <h1 class="text-uppercase font-weight-bold" style="font-family: 'Montserrat', sans-serif; font-size: 53px; line-height: 114px">IT RELAX</h1>
+            </div>
+            <div class="text-center">
+                 <p>Хочете бути в курсі усіх поїздок?</p>
+            </div>
         </div>
         <div class="col-4 mx-auto d-md-flex flex-md-row-reverse">
             <ul class="list-unstyled " style="font-family: 'Roboto', sans-serif;">
--- resources/views/layouts/style.blade.php
+++ resources/views/layouts/style.blade.php
@@ -2,9 +2,10 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
 <!-- Google Font: Source Sans Pro -->
+<link rel="stylesheet" href="/css/app.css">
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
 <!-- Font Awesome -->
-<link rel="stylesheet" href="{{asset('/admin/plugins/fontawesome-free/css/all.min.css"')}}>
+<link rel="stylesheet" href="{{asset('/admin/plugins/fontawesome-free/css/all.min.css"')}}">
 <!-- Ionicons -->
 <link rel="stylesheet" href="{{asset('https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css')}}">
 <!-- Tempusdominus Bootstrap 4 -->
@@ -30,105 +31,3 @@ crossorigin="anonymous" referrerpolicy="no-referrer" />
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
 
-<style>
-    footer{
-        position: relative;
-        height: 360px;
-        background-color: #177E1C;
-    }
-
-
-/*Clearing Floats*/
-.cf:before, .cf:after{
-    content:"";
-    display:table;
-}
-
-.cf:after{
-    clear:both;
-}
-
-.cf{
-    zoom:1;
-}
-/* Form wrapper styling */
-
-.search-wrapper {
-    width: 450px;
-    border-radius: 40px;
-    background-color: white;
-}
-
-/* Form text input */
-
-.search-wrapper input {
-    width: 330px;
-    height: 40px;
-    padding: 10px 30px;
-    float: left;
-    border: 0;
-    background: #fff;
-    border-radius: 40px;
-    border-top-style: none;
-}
-
-.search-wrapper input:focus {
-    outline: 0;
-    background: #fff;
-    box-shadow: 0 0 2px rgba(0,0,0,0.8) inset;
-}
-
-.search-wrapper input::-webkit-input-placeholder {
-    font-weight: normal;
-    font-style: italic;
-    padding-left: 20px;
-}
-
-.search-wrapper input:-moz-placeholder {
-    font-weight: normal;
-    font-style: italic;
-}
-
-.search-wrapper input:-ms-input-placeholder {
-    font-weight: normal;
-    font-style: italic;
-    border-style: none;
-}
-
-/* Form submit button */
-.search-wrapper button {
-    overflow: visible;
-    position: relative;
-    float: right;
-    border: 0;
-    padding: 0;
-    cursor: pointer;
-    height: 40px;
-    width: 110px;
-    font: 13px/40px 'lucida sans', 'trebuchet MS', 'Tahoma';
-    color: #fff;
-    background: #FF931ECC;
-    border-radius: 40px;
-}
-
-.search-wrapper button:hover{
-/*     background: #e54040; */
-}
-
-.search-wrapper button:active,
-.search-wrapper button:focus{
-    background: #198cff;
-    outline: 0;
-}
-
-.search-wrapper button:focus:before,
-.search-wrapper button:active:before{
-        border-right-color: #c42f2f;
-}
-
-.search-wrapper button::-moz-focus-inner { /* remove extra button spacing for Mozilla Firefox */
-    border: 0;
-    padding: 0;
-}
-
-</style>

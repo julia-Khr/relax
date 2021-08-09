8b6d8b0ebe56e1acbaacccd5aa9f556edd4bad90
Vickordon <vickordon@gmail.com>
2021-08-09 14:23:55 +0300

    addCarousel

--- routes/web.php
+++ routes/web.php
@@ -20,11 +20,11 @@ use App\Http\Controllers\VisitorController;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    return view('welcome');
+});
 
-// Auth::routes();
+Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 

dee8d6bebd8a07bff4b6eb031a44440054e85c95
libraln <libraolena@gmail.com>
2021-08-13 19:18:35 +0300

    Changes in navbar

--- resources/views/visitor/home/index.blade.php
+++ resources/views/visitor/home/index.blade.php
@@ -4,9 +4,6 @@
 @section('content')
   <div class="img__container">
     <img src="/img/tourist.png" class="img-fluid" alt="Описание изображения...">
-    <div class="img__description">
-      <div class="img__header">Заголовок для текста</div>
-      Текст поверх изображения...
     </div>
   </div>
 

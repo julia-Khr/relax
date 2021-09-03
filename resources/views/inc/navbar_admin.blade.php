bb0de4593787327d6bd0a199f46c30803dc11002
Vickordon <vickordon@gmail.com>
2021-09-03 21:19:10 +0300

    navbarPanel

---
+++ resources/views/inc/navbar_admin.blade.php
@@ -0,0 +1,29 @@
+ <!-- Navbar -->
+ <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+
+     <ul class="navbar-nav">
+         <li class="nav-item">
+             <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+         </li>
+         <li class="nav-item">
+             <a href="" class="nav-link">Головна</a>
+         </li>
+         <li class="nav-item">
+             <a href="" class="nav-link">Подорожі</a>
+         </li>
+         <li class="nav-item">
+             <a href="" class="nav-link">Історія подій</a>
+         </li>
+         <li class="nav-item">
+             <a href="" class="nav-link">Відгуки</a>
+         </li>
+         <li class="nav-item d-none d-sm-none d-md-block d-xxl-block d-xl-block d-lg-block">
+             <a href="" class="nav-link ">Контакти</a>
+         </li>
+         <li class="nav-item d-xxl-none d-xl-none d-lg-none d-md-none ">
+             <a href="" class="nav-link">Наші переваги</a>
+         </li>
+     </ul>
+
+
+ </nav>
